f82bceaf4bc1681c4dbdc61d58727eeb79eeaf8b
viktor hnatiuk <viktorgnatiuk136@gmail.com>
2026-03-30 21:18:42 +0300

    react hw 3 fix directorion app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,14 @@ import toast, { Toaster } from "react-hot-toast";
 
 import { useState } from "react";
 import "./App.module.css";
-import SearchBar from "./components/SearchBar/SearchBar";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import MovieModal from "./components/MovieModal/MovieModal";
-import { fetchMovies } from "./services/movieService";
+import SearchBar from "../SearchBar/SearchBar";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import { fetchMovies } from "../../services/movieService";
 
-import type { Movie } from "./types/movie";
+import type { Movie } from "../../types/movie";
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
